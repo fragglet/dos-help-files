d76bd88c42542813a7c36b91c2247eadd550c47d
Simon Howard <fraggle@gmail.com>
2023-07-26 00:32:43 -0400

    Don't include redirect pages in sitemap.
    This should fix the issue with Google Search not processing all the URLs
    in the sitemap because it is too large; the redirect pages get excluded
    by it anyway.

--- mksitemap.sh
+++ mksitemap.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 find *.hlp -type f -name '*.html' | while read filename; do
-	echo "https://fragglet.github.io/dos-help-files/$filename"
+	# Don't include redirect pages in the index:
+	if ! grep -q "meta http-equiv" "$filename"; then
+		echo "https://fragglet.github.io/dos-help-files/$filename"
+	fi
 done | sort -f > sitemap.txt
 
