2ea18b146cc7921b489dd1acb205dd2cd76b5277
Simon Howard <fraggle@soulsphere.org>
2025-11-30 23:32:39 -0500

    Update sitemap to new URL

--- mksitemap.sh
+++ mksitemap.sh
@@ -3,7 +3,7 @@
 find *.hlp -type f -name '*.html' | while read filename; do
 	# Don't include redirect pages in the index:
 	if ! grep -q "meta http-equiv" "$filename"; then
-		echo "https://fragglet.github.io/dos-help-files/$filename"
+		echo "https://dos-help.soulsphere.org/$filename"
 	fi
 done | sort -f > sitemap.txt
 
